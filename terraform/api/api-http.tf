a34d1f36443052e748af7d9c8a6ba97897f9185a
Paul Visscher <p.e.visscher@skyleague.io>
2024-09-01 20:32:55 +0200

    feat: refactor HTTP API authorizer configuration

--- terraform/api/api-http.tf
+++ terraform/api/api-http.tf
@@ -1,4 +1,3 @@
-
 locals {
   _http_api_stacks = {
     for stack, subs in local.starchart.stacks : stack => jsondecode(subs.runtime.deferred_http_api_input) if try(subs.runtime.deferred_http_api_input, null) != null
@@ -9,9 +8,29 @@ locals {
       account_id                   = toset([for stack, input in local._http_api_stacks : input.account_id if input.name == name])
       region                       = toset([for stack, input in local._http_api_stacks : input.region if input.name == name])
       disable_execute_api_endpoint = anytrue([for stack, input in local._http_api_stacks : input.disable_execute_api_endpoint if input.name == name])
+      request_authorizers = merge([
+        for stack, input in local._http_api_stacks : {
+          for name, authorizer in input.request_authorizers : "${stack}-${name}" => authorizer
+        } if input.name == name
+      ]...)
+      jwt_authorizers = merge([
+        for stack, input in local._http_api_stacks : {
+          for name, authorizer in input.jwt_authorizers : "${stack}-${name}" => authorizer
+        } if input.name == name
+      ]...)
       definition = {
         for path in toset(flatten([for stack, input in local._http_api_stacks : keys(input.definition)])) : path => {
-          for method in toset(flatten([for stack, input in local._http_api_stacks : try(keys(input.definition[path]), [])])) : method => [for stack, input in local._http_api_stacks : input.definition[path][method] if try(input.definition[path][method], null) != null][0]
+          for method in toset(flatten([for stack, input in local._http_api_stacks : try(keys(input.definition[path]), [])])) : method => [for stack, input in local._http_api_stacks : merge(
+            input.definition[path][method],
+            {
+              authorizer = try(input.definition[path][method].authorizer, null) != null ? merge(input.definition[path][method].authorizer, {
+                name = "${stack}-${input.definition[path][method].authorizer.name}"
+              }) : null
+              security = try({
+                for authorizer_name, scopes in input.definition[path][method].security : "${name}-${authorizer_name}" => scopes
+              }, null)
+            }
+          ) if try(input.definition[path][method], null) != null][0]
         }
       }
     }
@@ -29,6 +48,9 @@ module "http_api" {
   region     = tolist(each.value.region)[0]
   account_id = tolist(each.value.account_id)[0]
   definition = each.value.definition
+
+  request_authorizers = each.value.request_authorizers
+  jwt_authorizers = each.value.jwt_authorizers
 
   disable_execute_api_endpoint = each.value.disable_execute_api_endpoint
 }
--- terraform/runtime/http-api-settings/main.tf
+++ terraform/runtime/http-api-settings/main.tf
@@ -35,15 +35,10 @@ variable "request_authorizers" {
     ])
     ttl_in_seconds = optional(number, 60)
 
-    # request
     function_id             = optional(string)
-    function_name           = optional(string)
+    function_name           = string
     enable_simple_responses = optional(bool, false)
     payload_format_version  = optional(string, "2.0")
-
-    lambda = object({
-      function_name = string
-    })
   }))
   description = "Map of authorizers for the API"
   default     = {}
@@ -56,7 +51,7 @@ variable "jwt_authorizers" {
     security_scheme = optional(any)
     identity_source = optional(string, "$request.header.Authorization")
     ttl_in_seconds  = optional(number, 60)
-    # jwt
+    
     issuer   = optional(string)
     audience = optional(list(string))
   }))
@@ -96,34 +91,15 @@ output "disable_execute_api_endpoint" {
 
 output "request_authorizers" {
   value = {
-    for name, authorizer in var.request_authorizers : name => {
-      authorizerType                 = authorizer.type
-      identitySource                 = join(",", authorizer.identity_source)
-      resultTtlInSeconds             = authorizer.ttl_in_seconds
-      authorizerPayloadFormatVersion = authorizer.payload_format_version
-      enableSimpleResponses          = authorizer.enable_simple_responses
-      lambda                         = authorizer.lambda
-      # not sure if this is needed
-      # header = authorizer.header
-      securityScheme = authorizer.security_scheme
-    }
+    for name, authorizer in var.request_authorizers : name => merge(authorizer, {
+      identity_source = join(",", authorizer.identity_source)
+    })
   }
   description = "Map of authorizers for the API"
 }
 
 output "jwt_authorizers" {
-  value = {
-    for name, authorizer in var.jwt_authorizers : name => {
-      authorizerType     = authorizer.type
-      identitySource     = authorizer.identity_source
-      resultTtlInSeconds = authorizer.ttl_in_seconds
-
-      issuer   = authorizer.issuer
-      audience = authorizer.audience
-      
-      securityScheme = authorizer.security_scheme
-    }
-  }
+  value = var.jwt_authorizers
   description = "Map of authorizers for the API"
 }
 
