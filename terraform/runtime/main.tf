4f1688970d513febc4ca49e05696dbe29def2a41
MickVanDuijn <m.vanduijn@skyleague.io>
2024-09-10 15:25:27 +0200

    feat(cloudwatch): output log groups from runtime

--- terraform/runtime/main.tf
+++ terraform/runtime/main.tf
@@ -15,3 +15,11 @@ locals {
 # output "output" {
 #   value = var.starchart
 # }
+
+output "cloudwatch_log_groups" {
+  value = {
+    lambda   = local._cloudwatch_lambda
+    http_api = local._cloudwatch_http_api
+    rest_api = local._cloudwatch_rest_api
+  }
+}
