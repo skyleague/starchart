23c8f5ba6b00b975b48eb5f2ad26be4687f2fd0d
MickVanDuijn <m.vanduijn@skyleague.io>
2024-09-10 09:13:45 +0200

    fix(config-lambda): split handlers from formatted_handlers for plan-time keys in maps (#42)

--- terraform/modules/config-lambda/events.tf
+++ terraform/modules/config-lambda/events.tf
@@ -1,22 +1,26 @@
 locals {
   sqs_triggers = merge([
     for function_id, definition in local.handlers : {
-      for event in try(definition.events, []) : event.sqs.queueId => {
+      for idx, _event in try(definition.events, []) : _event.sqs.queueId => {
         function_id = function_id
-        batch_size  = try(event.sqs.batchSize, null)
-      } if try(event.sqs, null) != null
+        batch_size  = try(local.formatted_handlers[function_id].events[idx].sqs.batchSize, null)
+      } if try(_event.sqs, null) != null
     }
   ]...)
   eventbridge_to_sqs = merge([
     for function_id, definition in local.handlers : {
-      for event in try(definition.events, []) : event.sqs.queueId => {
-        event_bus_name = try(event.sqs.eventbridge.eventBusName, var.resources.eventbridge[event.sqs.eventbridge.eventBusId].id, null)
-        event_pattern  = try(jsonencode(event.sqs.eventbridge.eventPattern), null)
-      } if try(event.sqs.eventbridge, null) != null
+      for idx, _event in try(definition.events, []) : _event.sqs.queueId => {
+        event_bus_name = try(
+          local.formatted_handlers[function_id].events[idx].sqs.eventbridge.eventBusName,
+          var.resources.eventbridge[local.formatted_handlers[function_id].events[idx].sqs.eventbridge.eventBusId].id,
+          null
+        )
+        event_pattern = try(jsonencode(local.formatted_handlers[function_id].events[idx].sqs.eventbridge.eventPattern), null)
+      } if try(_event.sqs.eventbridge, null) != null
     }
   ]...)
   http_events = flatten([
-    for function_id, definition in local.handlers : [
+    for function_id, definition in local.formatted_handlers : [
       for event in try(definition.events, []) : {
         function_name = definition.function_name
         http          = event.http
@@ -25,36 +29,38 @@ locals {
   ])
   api_definition = {
     for http_path in toset(flatten([for event in local.http_events : event.http.path])) : http_path => {
-      for event in local.http_events : upper(event.http.method) => merge({
-        function_name = event.function_name
-      }, try(event.http.authorizer, null) != null ? { authorizer = event.http.authorizer } : {})
-      if event.http.path == http_path
+      for event in local.http_events : upper(event.http.method) => merge(
+        {
+          function_name = event.function_name
+        },
+        try(event.http.authorizer, null) != null ? { authorizer = event.http.authorizer } : {}
+      ) if event.http.path == http_path
     }
   }
   scheduled_events = merge({
-    for function_id, definition in local.handlers : "lambda-warmer-${definition.function_name}" => {
-      function_name = definition.function_name
+    for function_id, _definition in local.handlers : "lambda-warmer-${_definition.function_name}" => {
+      function_name = _definition.function_name
       function_id   = function_id
       schedule = {
-        rate        = try(definition.warmer.rate, "rate(5 minutes)")
-        description = try(definition.warmer.description, "Lambda warmer for ${definition.function_name}")
+        rate        = try(local.formatted_handlers[function_id].warmer.rate, "rate(5 minutes)")
+        description = try(local.formatted_handlers[function_id].warmer.description, "Lambda warmer for ${_definition.function_name}")
         input = try(
-          definition.warmer.input,
-          try(definition.warmer.inputPath, null) == null && try(definition.warmer.inputTransformer, null) == null
+          local.formatted_handlers[function_id].warmer.input,
+          try(_definition.warmer.inputPath, null) == null && try(_definition.warmer.inputTransformer, null) == null
           ? "__WARMER__"
           : null
         )
-        inputPath        = try(definition.warmer.inputPath, null)
-        inputTransformer = try(definition.warmer.inputTransformer, null)
+        inputPath        = try(local.formatted_handlers[function_id].warmer.inputPath, null)
+        inputTransformer = try(local.formatted_handlers[function_id].warmer.inputTransformer, null)
       }
-    } if try(definition.warmer.enabled, true) != false
+    } if try(_definition.warmer.enabled, true) != false
     }, [
-    for function_id, definition in local.handlers : {
-      for idx, event in try(definition.events, []) : try(event.ruleNamePrefix, "${function_id}-${idx}") => {
-        function_name = definition.function_name
+    for function_id, _definition in local.handlers : {
+      for idx, _event in try(_definition.events, []) : try(_event.ruleNamePrefix, "${function_id}-${idx}") => {
+        function_name = _definition.function_name
         function_id   = function_id
-        schedule      = event.schedule
-      } if try(event.schedule, null) != null
+        schedule      = local.formatted_handlers[function_id].events[idx].schedule
+      } if try(_event.schedule, null) != null
     }
   ]...)
 }
--- terraform/modules/config-lambda/main.tf
+++ terraform/modules/config-lambda/main.tf
@@ -3,27 +3,34 @@
 locals {
   _base_handlers = [
     for f in fileset(var.functions_dir, "functions/*/${var.handler_file}") : {
-      basedir = dirname(f)
-      definition = yamldecode(
-        templatefile(
-          "${var.functions_dir}/${f}",
-          merge(var.template_variables, local.resources_env)
-        )
-      )
+      basedir      = dirname(f)
+      handler_file = "${var.functions_dir}/${f}"
     }
   ]
   _handlers = [
     for h in local._base_handlers : {
-      basedir     = h.basedir
-      definition  = h.definition
-      function_id = try(h.definition.functionId, replace(replace(h.basedir, "functions/", ""), "/[^a-zA-Z0-9]+/", "-"))
+      basedir      = h.basedir
+      definition   = yamldecode(file(h.handler_file))
+      function_id  = try(h.definition.functionId, replace(replace(h.basedir, "functions/", ""), "/[^a-zA-Z0-9]+/", "-"))
+      handler_file = h.handler_file
     }
   ]
   handlers = {
     for h in local._handlers : h.function_id => merge(h.definition, {
       function_name = "${var.function_prefix}${try(h.definition.functionName, h.function_id)}"
       artifact_path = h.basedir
+      handler_file  = h.handler_file
     })
+  }
+  formatted_handlers = {
+    for fid, h in local.handlers : fid => merge(
+      h,
+      yamldecode(templatefile(h.handler_file, merge(var.template_variables, local.resources_env))),
+      {
+        function_name = h.function_name
+        artifact_path = h.artifact_path
+      }
+    )
   }
 }
 
