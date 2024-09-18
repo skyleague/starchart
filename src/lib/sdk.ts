82af493f5eb4792dd321968ef881b35faf65a3d8
MickVanDuijn <m.vanduijn@skyleague.io>
2024-09-18 08:56:27 +0200

    feat(sdk): support function-based client options in $sdk

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -2,7 +2,12 @@ import type { RestClientOptions } from '@skyleague/therefore/src/lib/primitives/
 import { StarchartConfiguration } from './configuration.js'
 import { openapi } from './openapi.js'
 
-export async function $sdk(options: Record<string, Partial<RestClientOptions> | undefined> = {}) {
+export async function $sdk(
+    options: Record<
+        string,
+        Partial<RestClientOptions> | ((args: { key: string }) => Partial<RestClientOptions>) | undefined
+    > = {},
+) {
     const { $restclient } = await import('@skyleague/therefore')
     const configuration = await StarchartConfiguration.load({})
     if ('left' in configuration) {
@@ -15,12 +20,16 @@ export async function $sdk(options: Record<string, Partial<RestClientOptions> | 
         Object.entries(openapiDocs)
             .filter(([, value]) => Object.keys(value.paths).length > 0)
             .map(([key, value]) => {
-                const clientOptions = options[key] ?? options._default ?? options.default
+                let clientOptions = options[key] ?? options._default ?? options.default
+                if (typeof clientOptions === 'function') {
+                    clientOptions = clientOptions({ key })
+                }
+
                 return [
                     `${key}Client`,
                     $restclient(value, {
-                        ...clientOptions,
                         filename: `${key}/rest.client.ts`,
+                        ...clientOptions,
                     }),
                 ]
             }),
