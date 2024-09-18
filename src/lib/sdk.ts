c50187090edeaae75853a3cce57bb8ac4b3e4f4e
MickVanDuijn <m.vanduijn@skyleague.io>
2024-09-18 08:53:41 +0200

    feat(sdk): add client options to $sdk function (#46)

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -1,7 +1,8 @@
+import type { RestClientOptions } from '@skyleague/therefore/src/lib/primitives/restclient/restclient.js'
 import { StarchartConfiguration } from './configuration.js'
 import { openapi } from './openapi.js'
 
-export async function $sdk() {
+export async function $sdk(options: Record<string, Partial<RestClientOptions> | undefined> = {}) {
     const { $restclient } = await import('@skyleague/therefore')
     const configuration = await StarchartConfiguration.load({})
     if ('left' in configuration) {
@@ -14,9 +15,11 @@ export async function $sdk() {
         Object.entries(openapiDocs)
             .filter(([, value]) => Object.keys(value.paths).length > 0)
             .map(([key, value]) => {
+                const clientOptions = options[key] ?? options._default ?? options.default
                 return [
                     `${key}Client`,
                     $restclient(value, {
+                        ...clientOptions,
                         filename: `${key}/rest.client.ts`,
                     }),
                 ]
