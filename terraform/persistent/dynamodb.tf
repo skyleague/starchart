d7a0742f75d46e23eb87fcb39c32191ad1edacd5
mkaracaoglu <121951240+mkaracaoglu@users.noreply.github.com>
2024-10-04 15:32:10 +0200

    refactor: add global secondary index in dynamodb.tf (#52)

--- terraform/persistent/dynamodb.tf
+++ terraform/persistent/dynamodb.tf
@@ -10,6 +10,22 @@ variable "dynamodb" {
         name = string
         type = optional(string, "S")
       }))
+      global_secondary_indexes = optional(map(object({
+        hash_key = object({
+          name = string
+          type = optional(string, "S")
+        })
+        range_key = optional(object({
+          name = string
+          type = optional(string, "S")
+        }))
+        projection_type    = optional(string, "KEYS_ONLY")
+        non_key_attributes = optional(set(string))
+        provisioned_capacity = optional(object({
+          read  = number
+          write = number
+        }))
+      })))
       deletion_protection_enabled = optional(bool, true)
     })
   )
@@ -22,9 +38,10 @@ module "dynamodb" {
   for_each = var.dynamodb
   source   = "git::https://github.com/skyleague/aws-dynamodb.git?ref=v3.0.0"
 
-  name      = each.value.name
-  hash_key  = each.value.hash_key
-  range_key = coalesce(each.value.range_key, null)
+  name                   = each.value.name
+  hash_key               = each.value.hash_key
+  range_key              = coalesce(each.value.range_key, null)
+  global_secondary_index = coalesce(each.value.global_secondary_index, null)
 
   deletion_protection_enabled = each.value.deletion_protection_enabled
 }
