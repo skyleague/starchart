5ea33d6e05fcba575ece93a74563abff51c12247
MickVanDuijn <m.vanduijn@skyleague.io>
2025-01-24 15:55:58 +0100

    fix: correctly pass config on bootstrap

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,13 +1,7 @@
 module "common" {
   source = "../modules/_common"
 
-  config = {
-    project_name       = local.starchart.config.project_name
-    project_identifier = local.starchart.config.project_identifier
-    environment        = local.starchart.config.environment
-    stack_name         = local.starchart.config.stack_name
-    repo_root          = local.starchart.config.repo_root
-  }
+  config = local.starchart.config
 
   default_tags = {}
 }
